aaf1e39ba6cf0cfb3f7c9eff90edb2927d9538a1
NPRohitkumar <nprohitkumar28@gmail.com>
2023-11-30 23:31:35 +0530

    factorial program

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int sum(int n);
+int main()
+{
+    int n,result;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    result=sum(n);
+    printf("fact of %d natural number is %d",n,result);
+    return 0;
+    
+
+}
+int sum(int n)
+{
+    if(n==0) {
+        return 1;
+    }
+    int factn=n*sum(n-1);
+
+}
