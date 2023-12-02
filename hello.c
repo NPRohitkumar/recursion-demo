a1707010b8bc7dad4c74cf99b442df4033da4b53
NPRohitkumar <nprohitkumar28@gmail.com>
2023-12-02 09:59:31 +0530

    var change

--- hello.c
+++ hello.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
-int sum(int n);
+int fact(int n);
 int main()
 {
     int n,result;
     printf("enter the value of n");
     scanf("%d",&n);
-    result=sum(n);
+    result=fact(n);
     printf("fact is %d",result);
     return 0;
     
 
 }
-int sum(int n)
+int fact(int n)
 {
     if(n==0) {
         return 1;
     }
-    int factn=n*sum(n-1);
+    return(n*fact(n-1));
 
 }
